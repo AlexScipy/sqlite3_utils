f7bfb40bfc1b4558994a0fded4511d26c421b079
AlexScipy <162141048+AlexScipy@users.noreply.github.com>
2024-03-11 16:25:19 +0100

    Delete init_db.py
    rimosso perchè ho caricato la versione aggiornata

--- init_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3, codecs
-
-connection = sqlite3.connect('database.db')
-with codecs.open('database.sql', encoding='utf-8') as f:
-    connection.executescript(f.read())
-connection.commit()
-connection.close()
