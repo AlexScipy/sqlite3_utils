e1fa4b272bf20e16dcadeb8dab24de6ec6c53800
AlexScipy <162141048+AlexScipy@users.noreply.github.com>
2024-03-13 10:19:18 +0100

    Update util_db.py

--- util_db.py
+++ util_db.py
@@ -7,74 +7,53 @@ class Database:
         self.db = db
 
     def connect(self):
-        connection = sqlite3.connect(self.db)  # Inizializzo connection con la connessione sqlite3 al db;
-        connection.row_factory = sqlite3.Row  # Imposto la connessione su Row in modo da poter utilizzare...
-        return connection
+        return sqlite3.connect(self.db)
 
-    def cursor(self):
-        return self.connect().cursor()
+    def cursor(self, connection):
+        return connection.cursor()
 
-    def commit(self):
-        return self.connect().commit()
+    def commit(self, connection):
+        connection.commit()
 
-    def close(self):
-        self.connect().close()
+    def close(self, connection):
+        connection.close()
 
 
-def get_tables(connection):
+def get_data_from_db_table(connection):
     cursor = connection.cursor()
-    cursor.execute(
-        "SELECT name FROM sqlite_master WHERE type='table';")  # query per ottenere i nomi di tutte le tabelle presenti nel database SQLite.
-    tables = cursor.fetchall()
-    cursor.close()
-    print("Tables Avaible:")
-    return [table[0] for table in tables]  # Ritorna una lista contenente solo i nomi delle tabelle
-
-
-def get_data_from_db_table(connection, table_name):
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
+    cursor.execute(f"SELECT * FROM accounts") #accounts è il nome specifico della tabella
     rows = cursor.fetchall()
     cursor.close()
     return [list(row) for row in rows]
 
 
-def update_user(connection, table_name):
-    username = input("Insert the username: ")
-    new_psw = input("new password: ")
+def update_user(connection, table_name, username, new_password):
     try:
         cursor = connection.cursor()
-        # Eseguo la query per selezionare l'ID dell'utente
-        cursor.execute(f"SELECT id FROM {table_name} WHERE (username) = '{username}';")
-        # Prendo l'ID dall'oggetto cursore
-        id_ = cursor.fetchone()[0]  # l'id è il primo campo nella riga
-        # Eseguo l'aggiornamento della password utilizzando l'ID ottenuto
-        cursor.execute(f"UPDATE {table_name} SET password_ = '{new_psw}', updated_at = CURRENT_TIMESTAMP WHERE id = {id_};")
+        cursor.execute(f"SELECT id FROM {table_name} WHERE username = ?", (username,))
+        id_ = cursor.fetchone()[0]
+        cursor.execute(f"UPDATE {table_name} SET password_ = ?, updated_at = ? WHERE id = ?",
+                       (new_password, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), id_))
         connection.commit()
-        return f"Password of: {username} successfully updated!\n"
+        return f"Password of {username} successfully updated!\n"
     except Exception as e:
         raise ValueError(f"Error occurred: {e}\n")
 
 
-def insert_user(connection, table_name):
-    username = input("username: ")
-    email = input("email: ")
-    password = input("password: ")
-    create_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    update_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def insert_user(connection, table_name, username, email, password):
     try:
+        create_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         cursor = connection.cursor()
         cursor.execute(
             f"INSERT INTO {table_name} (username, email, password_, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
-            (username, email, password, create_date, update_date))
+            (username, email, password, create_date, create_date))
         connection.commit()
         return "Insert done!\n"
     except Exception as e:
         raise ValueError(f"Error occurred: {e}\n")
 
 
-def remove_user(connection, table_name):
-    user_id = input("Insert the user 'id' to remove: ")
+def remove_user(connection, table_name, user_id):
     try:
         cursor = connection.cursor()
         cursor.execute(f"DELETE FROM {table_name} WHERE id = ?", (user_id,))
@@ -83,42 +62,3 @@ def remove_user(connection, table_name):
     except Exception as e:
         raise ValueError(f"Error occurred: {e}\n")
 
-
-if __name__ == "__main__":
-    db_file = input("INSERT DATABASE FILENAME: ")
-    db = Database(db_file)
-    connection = db.connect()
-
-    while True:
-        # Definisci un menu interattivo
-        print("\nMenu:")
-        print("1. Insert User")
-        print("2. Get Users")
-        print("3. Remove User")
-        print("4. Update User")
-        print("5. Exit")
-        choice = input("Select an option (1/2/3/4/5): ")
-
-        if choice == "1":
-            print(get_tables(connection))
-            table_name = input("INSERT TABLE NAME: ")
-            print(insert_user(connection, table_name))
-        elif choice == "2":
-            print(get_tables(connection))
-            table_name = input("INSERT TABLE NAME: ")
-            print(get_data_from_db_table(connection, table_name))
-        elif choice == "3":
-            print(get_tables(connection))
-            table_name = input("INSERT TABLE NAME: ")
-            print(remove_user(connection, table_name))
-        elif choice == "4":
-            print(get_tables(connection))
-            table_name = input("INSERT TABLE NAME: ")
-            print(update_user(connection, table_name))
-        elif choice.lower() == "5" or choice.lower() == "exit":
-            print("Exiting program...")
-            break
-        else:
-            print("Invalid choice! Please enter a valid option.")
-
-    connection.close()
