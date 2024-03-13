165626f213f556c5ae260480dd27eb1cd1059c27
AlexScipy <162141048+AlexScipy@users.noreply.github.com>
2024-03-13 23:37:56 +0100

    Update dynamic_app.py
    modificata la funzione handle_update_user()

--- dynamic_app.py
+++ dynamic_app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
-from util_db import Database, get_data_from_db_table, update_user, insert_user, remove_user
+from util_db import *
 
 app = Flask(__name__)
 db = Database("database.db")
+#usernames = [usr[1] for usr in get_data_from_db_table(db.connect())]
+#all_passwords_from_db = [psw[3] for psw in get_data_from_db_table(db.connect())]
+
 
 
 @app.route("/home", methods=['GET'])
@@ -13,11 +16,11 @@ def hello():
 @app.route('/insert_user', methods=['POST'])
 def handle_insert_user():
     try:
-        table_name = request.json['table_name']
+        #table_name = request.json['table_name']
         username = request.json['username']
-        email = request.json['email']
-        password = request.json['password']
-        return insert_user(db.connect(), table_name, username, email, password)
+        #email = request.json['email']
+        #password = request.json['password']
+        return insert_user(db.connect(), username)
     except Exception as e:
         print(e)
         return jsonify({'error': str(e)}), 500
@@ -35,9 +38,10 @@ def handle_get_users():
 @app.route('/remove_user', methods=['DELETE'])
 def handle_remove_user():
     try:
-        table_name = request.json['table_name']
-        user_id = request.json['user_id']
-        return remove_user(db.connect(), table_name, user_id)
+        username = request.json['username']
+        password = request.json['password']
+        #usernames = [usr[1] for usr in get_data_from_db_table(db.connect())]
+        return remove_user(db.connect(), username, password)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -45,17 +49,10 @@ def handle_remove_user():
 @app.route('/update_user', methods=['PUT'])
 def handle_update_user():
     try:
-        table_name = request.json['table_name']
         username = request.json['username']
-        password = request.json['old_password']
+        old_password = request.json['old_password']
         new_password = request.json['new_password']
-        # verifico che la password vecchia inserita sia la stessa presente nel db
-        all_passwords_from_db = [psw[3] for psw in get_data_from_db_table(db.connect(), table_name)]
-        print(all_passwords_from_db)
-        if password in all_passwords_from_db:
-            return update_user(db.connect(), table_name, username, new_password)
-        else:
-            return jsonify({'error': "password not matched in the db..."})
+        return update_user(db.connect(), username, old_password, new_password)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
